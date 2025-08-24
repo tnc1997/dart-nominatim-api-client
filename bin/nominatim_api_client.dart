4a3435747745b25ab03e1152cfd4bcb3ee475ae0
Thomas Clark <tnc1997@outlook.com>
2025-08-24 19:23:41 +0100

    chore: initial commit

---
+++ bin/nominatim_api_client.dart
@@ -0,0 +1,5 @@
+import 'package:nominatim_api_client/nominatim_api_client.dart' as nominatim_api_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nominatim_api_client.calculate()}!');
+}
---
+++ test/nominatim_api_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:nominatim_api_client/nominatim_api_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
