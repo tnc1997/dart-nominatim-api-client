a388947b718f1fb2d66a3637cd2afbbe81ef9e36
Thomas Clark <tnc1997@outlook.com>
2025-09-02 21:00:34 +0100

    refactor: add library and export directives

--- lib/nominatim_api_client.dart
+++ lib/nominatim_api_client.dart
@@ -1,3 +1,9 @@
-int calculate() {
-  return 6 * 7;
-}
+library nominatim_api_client;
+
+export 'src/clients/email_address_client.dart';
+export 'src/clients/user_agent_client.dart';
+export 'src/constants/uri_constants.dart';
+export 'src/exceptions/nominatim_api_client_exception.dart';
+export 'src/models/geo_json.dart';
+export 'src/models/search_result.dart';
+export 'src/nominatim_api_client_base.dart';
