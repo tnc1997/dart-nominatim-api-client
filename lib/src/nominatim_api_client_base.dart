25d533dbee6363a0586223b49b32182725f60d90
Thomas Clark <tnc1997@outlook.com>
2025-09-18 11:12:29 +0100

    feat: add reverse endpoint

--- lib/src/nominatim_api_client_base.dart
+++ lib/src/nominatim_api_client_base.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 
 import 'constants/uri_constants.dart';
 import 'exceptions/nominatim_api_client_exception.dart';
+import 'models/reverse_result.dart';
 import 'models/search_result.dart';
 
 class NominatimApiClient {
@@ -12,6 +13,53 @@ class NominatimApiClient {
   NominatimApiClient({
     http.Client? client,
   }) : _client = client ?? http.Client();
+
+  /// Reverse geocoding generates an address from a coordinate given as latitude and longitude.
+  Future<ReverseResult> reverse(
+    double lat,
+    double lon, {
+    bool? addressDetails,
+    bool? extraTags,
+    bool? nameDetails,
+    int? zoom,
+    List<String>? layer,
+    bool? polygonGeoJson,
+    bool? polygonKml,
+    bool? polygonSvg,
+    bool? polygonText,
+    double? polygonThreshold,
+    bool? debug,
+  }) async {
+    final response = await _client.get(
+      Uri.https(
+        authority,
+        '/reverse',
+        {
+          'format': 'jsonv2',
+          'lat': lat.toString(),
+          'lon': lon.toString(),
+          if (addressDetails != null)
+            'addressdetails': addressDetails ? '1' : '0',
+          if (extraTags != null) 'extratags': extraTags ? '1' : '0',
+          if (nameDetails != null) 'namedetails': nameDetails ? '1' : '0',
+          if (zoom != null) 'zoom': zoom.toString(),
+          if (layer != null) 'layer': layer.join(','),
+          if (polygonGeoJson != null)
+            'polygon_geojson': polygonGeoJson ? '1' : '0',
+          if (polygonKml != null) 'polygon_kml': polygonKml ? '1' : '0',
+          if (polygonSvg != null) 'polygon_svg': polygonSvg ? '1' : '0',
+          if (polygonText != null) 'polygon_text': polygonText ? '1' : '0',
+          if (polygonThreshold != null)
+            'polygon_threshold': polygonThreshold.toString(),
+          if (debug != null) 'debug': debug ? '1' : '0',
+        },
+      ),
+    );
+
+    NominatimApiClientException.checkIsSuccessStatusCode(response);
+
+    return ReverseResult.fromJson(json.decode(response.body));
+  }
 
   /// The search API allows you to look up a location from a textual description or address. Nominatim supports structured and free-form search queries.
   Future<List<SearchResult>> search({
